dace0384be7029a36dc4bd5d43e91ae36bc9f4b5
Alan Hollis <alan.hollis@virgin.net>
2011-05-30 16:29:08 +0100

    Upgrading to latest jquery mobile version

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel=stylesheet href="http://code.jquery.com/mobile/1.0a3/jquery.mobile-1.0a3.min.css"/>
 <link rel=stylesheet href="./styles/style-comments.css"/>
-<script src="http://code.jquery.com/jquery-1.5.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.0a3/jquery.mobile-1.0a3.min.js"></script>
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css" />
+<script src="http://code.jquery.com/jquery-1.5.2.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.js"></script>
 <script src="./mobileReddit.js"></script>
 </head>
 <body>
