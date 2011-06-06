4782b0347cb0def59ca19994c94509b03cb4428a
Alan Hollis <alan.hollis@virgin.net>
2011-06-06 19:04:02 +0100

    Using latest version of jquery mobile

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 <html>
 <head>
 <link rel=stylesheet href="./styles/style-comments.css"/>
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css" />
-<script src="http://code.jquery.com/jquery-1.5.2.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.js"></script>
+<link href="http://code.jquery.com/mobile/latest/jquery.mobile.min.css" rel="stylesheet" type="text/css" />
+<script src="http://code.jquery.com/jquery-1.6.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/latest/jquery.mobile.min.js"></script>
 <script src="./mobileReddit.js"></script>
 </head>
 <body>
